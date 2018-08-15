cb8d42393a30033f39614d1c878b223075992f29
davidhardson <davidhardson@hotmail.com>
2018-08-15 13:13:34 +0800

    删除无用的sql

--- msa-sso/src/main/java/com/eshequ/msa/sso/mapper/SsoMenuMapper.java
+++ msa-sso/src/main/java/com/eshequ/msa/sso/mapper/SsoMenuMapper.java
@@ -1,17 +1,8 @@
 package com.eshequ.msa.sso.mapper;
 
+import com.eshequ.msa.common.CommonMapper;
 import com.eshequ.msa.sso.entity.SsoMenu;
 
-public interface SsoMenuMapper {
-    int deleteByPrimaryKey(Long menuId);
-
-    int insert(SsoMenu record);
-
-    int insertSelective(SsoMenu record);
-
-    SsoMenu selectByPrimaryKey(Long menuId);
-
-    int updateByPrimaryKeySelective(SsoMenu record);
-
-    int updateByPrimaryKey(SsoMenu record);
+public interface SsoMenuMapper extends CommonMapper<SsoMenu>{
+    
 }
--- msa-sso/src/main/java/com/eshequ/msa/sso/mapper/SsoRoleMapper.java
+++ msa-sso/src/main/java/com/eshequ/msa/sso/mapper/SsoRoleMapper.java
@@ -1,17 +1,8 @@
 package com.eshequ.msa.sso.mapper;
 
+import com.eshequ.msa.common.CommonMapper;
 import com.eshequ.msa.sso.entity.SsoRole;
 
-public interface SsoRoleMapper {
-    int deleteByPrimaryKey(Long roleId);
-
-    int insert(SsoRole record);
-
-    int insertSelective(SsoRole record);
-
-    SsoRole selectByPrimaryKey(Long roleId);
-
-    int updateByPrimaryKeySelective(SsoRole record);
-
-    int updateByPrimaryKey(SsoRole record);
+public interface SsoRoleMapper extends CommonMapper<SsoRole> {
+    
 }
