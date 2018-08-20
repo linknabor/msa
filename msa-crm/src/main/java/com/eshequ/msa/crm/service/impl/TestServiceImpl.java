2e88f96fbf5ce6f0a81ab90a549b71064e1cb71c
davidhardson <davidhardson@hotmail.com>
2018-08-20 17:15:40 +0800

    BaseModel.java 和 CommonMapper.java 移动到common下

--- msa-crm/src/main/java/com/eshequ/msa/crm/service/impl/TestServiceImpl.java
+++ msa-crm/src/main/java/com/eshequ/msa/crm/service/impl/TestServiceImpl.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import com.eshequ.msa.crm.mapper.SpServeBillPayTradeMapper;
+import com.eshequ.msa.crm.model.SpServeBillPayTrade;
 //import com.eshequ.msa.sso.mapper.SpServeBillPayTradeMapper;
 //import com.eshequ.msa.sso.model.SpServeBillPayTrade;
 import com.eshequ.msa.crm.service.TestService;
@@ -19,8 +21,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class TestServiceImpl implements TestService {
 
-//	@Autowired
-//	private SpServeBillPayTradeMapper billPayTradeMapper;
+	@Autowired
+	private SpServeBillPayTradeMapper billPayTradeMapper;
 	
 	@Autowired
 	private SnowFlake snowFlag;
@@ -31,18 +33,18 @@ public class TestServiceImpl implements TestService {
 	@Override
 	public String testQuery(String num) {
 		String retStr = "";
-//		if (StringUtils.isEmpty(num)) {
-//			 retStr = "no params!";
-//		}else {
-//			List<SpServeBillPayTrade> list = billPayTradeMapper.getBillTradeByCsp("130409100000004002");
-//			ObjectMapper mapper = new ObjectMapper();
-//			try {
-//				retStr = mapper.writeValueAsString(list.get((0)));
-//			} catch (JsonProcessingException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//		}
+		if (StringUtils.isEmpty(num)) {
+			 retStr = "no params!";
+		}else {
+			List<SpServeBillPayTrade> list = billPayTradeMapper.getBillTradeByCsp("130409100000004002");
+			ObjectMapper mapper = new ObjectMapper();
+			try {
+				retStr = mapper.writeValueAsString(list.get((0)));
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		return retStr;
 		
 	}
