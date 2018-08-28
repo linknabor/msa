610ba20cc53f99f50df4e52325f05e4ce4589a43
davidhardson <davidhardson@hotmail.com>
2018-08-28 17:02:30 +0800

    去除中文

--- msa-common/src/main/java/com/eshequ/msa/codes/CodesItem.java
+++ msa-common/src/main/java/com/eshequ/msa/codes/CodesItem.java
@@ -15,78 +15,80 @@ import com.eshequ.msa.codes.model.CodeInfo;
 import com.eshequ.msa.exception.NotFoundException;                                           
 @Component                                                                                   
 public class CodesItem {                                                      			
-	/**单位状态   */
+	
 	protected static final String orgStatus=new String("50");
-	/**发票开具类型   */
+	
 	protected static final String invoiceType=new String("52");
-	/**发票开具状态   */
+	
 	protected static final String invoiceStatus=new String("53");
-	/**发票申请类型   */
+	
 	protected static final String invoiceApplyType=new String("54");
-	/**注册信息状态   */
+	
 	protected static final String regInfoStatus=new String("55");
-	/**机构类别   */
+	
 	protected static final String orgType=new String("56");
-	/**机构状态   */
-	protected static final String orgStart=new String("57");
-	/**物业项目性质   */
+	
 	protected static final String proCreateType=new String("18");
-	/**物业项目类型   */
+	
 	protected static final String sectType=new String("19");
-	/**状态   */
+	
 	protected static final String infoStatus=new String("20");
-	/**建筑类型   */
+	
 	protected static final String buildType=new String("21");
-	/**是否标志   */
+	
 	protected static final String isFlag=new String("22");
-	/**户型   */
+	
 	protected static final String houseModel=new String("23");
-	/**车位类型   */
+	
 	protected static final String carportType=new String("24");
-	/**朝向   */
+	
 	protected static final String ward=new String("25");
-	/**商铺类型   */
+	
 	protected static final String shopsType=new String("26");
-	/**广告位类型   */
+	
 	protected static final String adLocateType=new String("27");
-	/**物业单元性质   */
+	
 	protected static final String mngCellKind=new String("28");
-	/**物业产权性质   */
+	
 	protected static final String housePropertyKind=new String("29");
-	/**电梯标志   */
+	
 	protected static final String liftFlag=new String("30");
-	/**建筑用途   */
+	
 	protected static final String buildUsage=new String("31");
-	/**物业单元类型   */
+	
 	protected static final String mngCellType=new String("32");
-	/**停车类型   */
+	
 	protected static final String parkType=new String("33");
-	/**车辆类型   */
+	
 	protected static final String carType=new String("34");
-	/**颜色   */
+	
 	protected static final String colour=new String("35");
-	/**车辆归属   */
+	
 	protected static final String carOwnerType=new String("36");
-	/**证件类型   */
+	
 	protected static final String certType=new String("38");
-	/**客户类型   */
+	
 	protected static final String custType=new String("39");
-	/**客户性质   */
+	
 	protected static final String custKind=new String("40");
-	/**支付渠道   */
+	
 	protected static final String payMethod=new String("41");
-	/**支付平台   */
+	
 	protected static final String platChannel=new String("42");
-	/**商户类型   */
+	
 	protected static final String mchType=new String("43");
-	/**支付交易状态   */
+	
 	protected static final String tradeState=new String("44");
-	/**退款交易状态   */
+	
 	protected static final String refundState=new String("45");
-	/**产品版本   */
+	
 	protected static final String productVersion=new String("46");
-	/**银行卡类型   */
+	
 	protected static final String cardType=new String("49");
+	
+	protected static final String smsStatus=new String("58");
+	
+	protected static final String menuFunLevel=new String("59");
 
 
 	protected static final Map mapCat= new HashMap(100);
@@ -97,7 +99,6 @@ public class CodesItem {
 		mapCat.put("InvoiceApplyType",invoiceApplyType);
 		mapCat.put("RegInfoStatus",regInfoStatus);
 		mapCat.put("OrgType",orgType);
-		mapCat.put("OrgStart",orgStart);
 		mapCat.put("ProCreateType",proCreateType);
 		mapCat.put("SectType",sectType);
 		mapCat.put("InfoStatus",infoStatus);
@@ -127,6 +128,8 @@ public class CodesItem {
 		mapCat.put("RefundState",refundState);
 		mapCat.put("ProductVersion",productVersion);
 		mapCat.put("CardType",cardType);
+		mapCat.put("SmsStatus",smsStatus);
+		mapCat.put("MenuFunLevel",menuFunLevel);
 	}
 
 
@@ -147,7 +150,6 @@ public class CodesItem {
 	 * @param category   本代码所属的类别的编号                                                       
 	 * @param code   本代码的代码值                                                                   
 	 * @return                                                                                        
-	 * @throws                                                                                        
 	 */                                                                                               
 	protected static String getValue(String category,String code){                                    
 		Map map=(Map)mapCategoryCode.get(category);                                                     
@@ -189,8 +191,8 @@ public class CodesItem {
 		   return mp;
 		}
 
-	/**直接转换成中文名字                                                                             
-	 * @无参数                                                                                        
+	/**                                                                                               
+	 * 直接转换成中文名字                                                                             
 	 * @return                                                                                        
 	 */                                                                                               
 	public  String getValue(){                                                                        
@@ -207,8 +209,6 @@ public class CodesItem {
 	 * 构造函数.                                                                                      
 	 * @param category   本代码所属的类别的编号                                                       
 	 * @param code   本代码的代码值                                                                   
-	 * @return                                                                                        
-	 * @throws                                                                                        
 	 */                                                                                               
 	protected  CodesItem(String category,String code){                                                
 		this.category=category;                                                                         
