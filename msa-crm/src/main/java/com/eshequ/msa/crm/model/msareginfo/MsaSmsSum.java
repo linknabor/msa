a2f20efdb924663479fbbad9a12ee94db24adde4
bobcjx <15007336137@163.com>
2018-08-30 15:00:24 +0800

    短信发送

---
+++ msa-crm/src/main/java/com/eshequ/msa/crm/model/msareginfo/MsaSmsSum.java
@@ -0,0 +1,56 @@
+package com.eshequ.msa.crm.model.msareginfo;
+
+import com.eshequ.msa.common.BaseModel;
+import java.math.BigDecimal;
+
+public class MsaSmsSum extends BaseModel {
+    private Long smsBatch;
+
+    private BigDecimal totalCount;
+
+    private String sendDate;
+
+    private String sendTime;
+
+    private BigDecimal totalFailed;
+
+    public Long getSmsBatch() {
+        return smsBatch;
+    }
+
+    public void setSmsBatch(Long smsBatch) {
+        this.smsBatch = smsBatch;
+    }
+
+    public BigDecimal getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(BigDecimal totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public String getSendDate() {
+        return sendDate;
+    }
+
+    public void setSendDate(String sendDate) {
+        this.sendDate = sendDate;
+    }
+
+    public String getSendTime() {
+        return sendTime;
+    }
+
+    public void setSendTime(String sendTime) {
+        this.sendTime = sendTime;
+    }
+
+    public BigDecimal getTotalFailed() {
+        return totalFailed;
+    }
+
+    public void setTotalFailed(BigDecimal totalFailed) {
+        this.totalFailed = totalFailed;
+    }
+}
