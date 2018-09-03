69051db7bb3fab3d0ed81d2ca1794601aac4231a
davidhardson <davidhardson@hotmail.com>
2018-09-03 17:00:51 +0800

    crm包结构调整

--- msa-crm/src/main/java/com/eshequ/msa/crm/mapper/sysmapper/MsaCfgSysParaMapper.java
+++
@@ -1,8 +0,0 @@
-package com.eshequ.msa.crm.mapper.sysmapper;
-
-import com.eshequ.msa.common.CommonMapper;
-import com.eshequ.msa.crm.model.sysmodel.MsaCfgSysPara;
-
-public interface MsaCfgSysParaMapper extends CommonMapper<MsaCfgSysPara>{
-
-}
