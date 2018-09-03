122be989708d77eeca18287cc2b9944be199c7ac
davidhardson <davidhardson@hotmail.com>
2018-09-03 17:32:42 +0800

    init ops

---
+++ msa-ops/src/main/java/com/eshequ/msa/ops/model/msareginfo/MsaRegInfo.java
@@ -0,0 +1,165 @@
+package com.eshequ.msa.ops.model.msareginfo;
+
+import com.eshequ.msa.common.BaseModel;
+
+public class MsaRegInfo extends BaseModel {
+    private String regEnterpriseId;
+
+    private String status;
+
+    private String regDate;
+
+    private String regTime;
+
+    private String randomCode;
+
+    private String loginName;
+
+    private String enterpriseName;
+
+    private String linkMan;
+
+    private String orgTel;
+
+    private String email;
+
+    private String postCode;
+
+    private String orgAddr;
+
+    private String remark;
+
+    private String productVersion;
+
+    private String startDate;
+
+    private String backTeName;
+
+    public String getRegEnterpriseId() {
+        return regEnterpriseId;
+    }
+
+    public void setRegEnterpriseId(String regEnterpriseId) {
+        this.regEnterpriseId = regEnterpriseId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public String getRegTime() {
+        return regTime;
+    }
+
+    public void setRegTime(String regTime) {
+        this.regTime = regTime;
+    }
+
+    public String getRandomCode() {
+        return randomCode;
+    }
+
+    public void setRandomCode(String randomCode) {
+        this.randomCode = randomCode;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getEnterpriseName() {
+        return enterpriseName;
+    }
+
+    public void setEnterpriseName(String enterpriseName) {
+        this.enterpriseName = enterpriseName;
+    }
+
+    public String getLinkMan() {
+        return linkMan;
+    }
+
+    public void setLinkMan(String linkMan) {
+        this.linkMan = linkMan;
+    }
+
+    public String getOrgTel() {
+        return orgTel;
+    }
+
+    public void setOrgTel(String orgTel) {
+        this.orgTel = orgTel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getOrgAddr() {
+        return orgAddr;
+    }
+
+    public void setOrgAddr(String orgAddr) {
+        this.orgAddr = orgAddr;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getProductVersion() {
+        return productVersion;
+    }
+
+    public void setProductVersion(String productVersion) {
+        this.productVersion = productVersion;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getBackTeName() {
+        return backTeName;
+    }
+
+    public void setBackTeName(String backTeName) {
+        this.backTeName = backTeName;
+    }
+}
