61bb94d08d288692cd26da50ddb0c51368cb9043
bobcjx <15007336137@163.com>
2018-09-04 10:34:42 +0800

    统一返回类型为BaseResult<?>

--- msa-ops/src/main/java/com/eshequ/msa/ops/service/msareginfo/MsaRegInfoService.java
+++ msa-ops/src/main/java/com/eshequ/msa/ops/service/msareginfo/MsaRegInfoService.java
@@ -6,18 +6,19 @@ import java.util.Map;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.eshequ.msa.common.BaseResult;
 import com.eshequ.msa.ops.model.msareginfo.MsaRegInfo;
 import com.eshequ.msa.ops.model.msareginfo.MsaSmsSum;
 
 public interface MsaRegInfoService {
 	// 添加企业注册信息
-	int addMsaInfo(MsaRegInfo masRegInfo);
+	BaseResult<?> addMsaInfo(MsaRegInfo masRegInfo);
 
 	// 导入excel文件保存信息
-	Map<String, String> importData(MultipartFile file) throws IOException;
+	BaseResult<?> importData(MultipartFile file) throws IOException;
 
 	// 根据导入批次号发送短信
-	Map<String, Object> sendSms(String importBatchs);
+	BaseResult<?> sendSms(String importBatchs);
 
 	// 根据日期查询短信信息
 	List<MsaSmsSum> findMsaSmsSum(String date);
