39dc3e9889282fe6a8e57e618572d3d81af29e09
davidhardson <davidhardson@hotmail.com>
2018-09-10 14:01:51 +0800

    init bdp

---
+++ msa-bdp/src/main/java/com/eshequ/msa/ops/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.eshequ.msa.ops.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+//多数据源需要exclude后面那2个类，不然spring会自动配置一个数据源
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})	
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages={"com.eshequ.msa.bdp"})
+@ComponentScan("com.eshequ.msa")
+public class AppConfig {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppConfig.class, args);
+	}
+
+}
