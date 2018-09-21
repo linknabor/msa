5331d8bcc18c972c5a627449337541869452d20f
shengguangyuan <shengguangyuan@126.com>
2018-09-21 17:49:14 +0800

    20180921

--- msa-dms/src/main/java/com/eshequ/msa/dms/mapper/msa/MsaBaseCityMapper.java
+++ msa-dms/src/main/java/com/eshequ/msa/dms/mapper/msa/MsaBaseCityMapper.java
@@ -1,8 +1,8 @@
 package com.eshequ.msa.dms.mapper.msa;
 
 import com.eshequ.msa.common.CommonMapper;
-import com.eshequ.msa.dms.model.msa.MsaBaseEnterprise;
+import com.eshequ.msa.dms.model.msa.MsaBaseCity;
 
-public interface MsaBaseCityMapper extends CommonMapper<MsaBaseEnterprise> {
+public interface MsaBaseCityMapper extends CommonMapper<MsaBaseCity> {
 
 }
