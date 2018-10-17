7e0600f6c2d39c20db001634922e6b6ff195de6f
shengguangyuan <shengguangyuan@126.com>
2018-10-17 20:11:31 +0800

    20181017

---
+++ msa-dms/src/main/java/com/eshequ/msa/dms/service/transfer/impl/DataTransferServiceImpl.java
@@ -0,0 +1,20 @@
+package com.eshequ.msa.dms.service.transfer.impl;
+
+import com.eshequ.msa.common.BaseResult;
+import com.eshequ.msa.dms.service.transfer.DataTransferService;
+
+public class DataTransferServiceImpl implements DataTransferService{
+
+	@Override
+	public BaseResult<String> migrateSect() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BaseResult<String> migrateHouse() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
