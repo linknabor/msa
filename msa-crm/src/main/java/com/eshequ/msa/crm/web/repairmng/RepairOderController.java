3d4e671ab9a0e00116e2869a5d36116f3787a914
bobcjx <15007336137@163.com>
2018-11-01 17:02:13 +0800

    增加报修订单分页

--- msa-crm/src/main/java/com/eshequ/msa/crm/web/repairmng/RepairOderController.java
+++ msa-crm/src/main/java/com/eshequ/msa/crm/web/repairmng/RepairOderController.java
@@ -1,6 +1,7 @@
 package com.eshequ.msa.crm.web.repairmng;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -9,15 +10,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eshequ.msa.common.BaseResult;
 import com.eshequ.msa.crm.model.repairmng.RepairAssign;
 import com.eshequ.msa.crm.model.repairmng.RepairOrder;
 import com.eshequ.msa.crm.service.repairmng.RepairOrderService;
-import com.eshequ.msa.crm.util.QiYeWeiXinUtil;
 import com.eshequ.msa.crm.vo.repairmng.RepairAndFileVo;
 import com.eshequ.msa.crm.web.BaseController;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 @RestController
 @RequestMapping("/repairOder")
@@ -29,8 +32,12 @@ public class RepairOderController extends BaseController {
 
 	// 获取报修订单列表
 	@RequestMapping(value = "/getRepairOrderList", method = RequestMethod.GET)
-	public BaseResult<?> getRepairOrderList(String userId) {
-		return BaseResult.successResult(repairOrderService.getRepairOrderList(userId));
+	public BaseResult<?> getRepairOrderList(@RequestParam(defaultValue = "0", required = false) Integer pageNum,
+			@RequestParam(defaultValue = "10", required = false) Integer pageSize,String userId) {
+		PageHelper.startPage(pageNum, pageSize);
+		List<RepairOrder> lists =repairOrderService.getRepairOrderList(userId);
+		PageInfo<RepairOrder> pageInfo = new PageInfo<>(lists);
+		return BaseResult.successResult(pageInfo);
 	}
 
 	// 报修订单详情
